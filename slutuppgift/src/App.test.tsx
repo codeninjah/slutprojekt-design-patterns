f981900c300e0bcdcc599197bc9d5575ade02105
codeninjah <codeninjahblog@gmail.com>
2022-01-13 14:23:14 +0100

    One more commit

--- slutuppgift/src/App.test.tsx
+++ slutuppgift/src/App.test.tsx
@@ -9,5 +9,18 @@ import MeetupItem from './components/MeetupItem';
 describe("Testar med enzyme", () => {
   it("renders the App component", () => {
     render(<App />);
+  }),
+    
+  it("kommentar skrivs ut på rätt event när man skrivit ett och tryckt på skicka knappen", () => {
+    const wrapper = mount(<App />)
+    //const wrapper = mount(<MeetupItem id={'0123456'} name={'SpaceX'} date={"2021-02-26"} description={"SpaceX is already a success, with people sent into space. This meetup will feature a lecture about how the program plans to build bases on the Moon and on March."} comments={[]} addComment={function (id: string, comment: string): void {} } />)
+    const registerBtn = wrapper.find('[data-test="attend-btn"]').at(0)
+    registerBtn.simulate('click')
+    const input = wrapper.find('[data-test="meetups-comment-field"]').at(0)
+    const commentsBtn = wrapper.find('[data-test="meetups-comment-btn"]')
+    input.simulate('change', {target: {value: 'LOV3333'} })
+    commentsBtn.simulate('click')
+    const comment = wrapper.find('[data-test="comment-item"]')
+    expect(comment.exists()).toBe(true)
   })
 });
--- slutuppgift/src/components/MeetupItem.tsx
+++ slutuppgift/src/components/MeetupItem.tsx
@@ -29,7 +29,7 @@ const MeetupItem = ({id, name, date, description, comments, addComment}:props) =
                 <h1>{name}</h1>
                 
                 { showDetails ? <div><h3>Welcome to our event!</h3> 
-                    <p><img src="https://www.seekpng.com/png/detail/9-93808_golden-stars-png-golden-star-transparent.png"></img></p>
+                    <p><img src="https://upload.wikimedia.org/wikipedia/commons/9/9c/Golden_star.svg"></img></p>
                 <p className="event-details">We hope you enjoy it, we cannot wait for you to experience it and please do not forget to leave a comment or two</p></div>
                 : <></>}
 
