30cafa7e20921ece8b5ffafa8de7b4656c4e6573
codeninjah <codeninjahblog@gmail.com>
2022-02-04 22:25:25 +0100

    Progress

--- slutuppgift/src/App.tsx
+++ slutuppgift/src/App.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { MeetupsView } from '../src/components/MeetupsView'
-import  MeetupItem  from '../src/components/MeetupItem';
-import { IMeetup } from './models/Meetup';
 
 function App() {
   return (
--- slutuppgift/src/components/MeetupItem.test.tsx
+++ slutuppgift/src/components/MeetupItem.test.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import MeetupItem from './MeetupItem';
-import MeetupsView from './MeetupsView';
 
 
-describe("Should ", () => {
-    it("something happens when clicking the register button", () => {
+describe("Should", () => {
+    it("do something happens when clicking the register button", () => {
         const wrapper = mount(<MeetupItem id={'0123456789'} name={'CryptoTalks'} date={"2022-01-26"} description={"Bitcoin or Dogecoin? How does Crypto Currency work? What's Bitcoin mining? Why can a Bitcoin cost $10,000 sometimes and plunge to $3,000? Which currency is the next Bitcoin? How does it change banking and finance?"} comments={[]} addComment={function (id: string, comment: string): void {} } />)
         const registerBtn = wrapper.find('[data-test="attend-btn"]')
         registerBtn.simulate('click')
